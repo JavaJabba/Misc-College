e964e48e554d504a08fdf14d603ecedac974e8ab
JavaJabba <dylanmurray1997@gmail.com>
2024-03-19 00:27:03 +0000

    ran out of time

--- memoryManagement.py
+++ memoryManagement.py
@@ -1,10 +1,12 @@
 '''
-Circular Linked List Memory Management System
+Two Handed Clock Memory Management System
+Dylan Murray - 121747725
 '''
-class CircularLinkedList:
+class clock:
 
       def __init__(self):
             self.tail = None
+            self.resetlist = []
       
       def addNode(self, process):
             # If the list is empty the initialise list.
@@ -12,6 +14,8 @@ class CircularLinkedList:
                   return self.addWhenEmpty(process)
             # Else, Assign new node to beginning of the list as most recent item.
             temp = Node(process)
+            # Add node to reset list.
+            self.resetlist.append(temp)
             temp.next = self.tail.next
             self.tail.next = temp
             return self.tail
@@ -24,16 +28,55 @@ class CircularLinkedList:
             self.tail.next = self.tail
             return self.tail
       
-      def traverse(self): 
-        if (self.tail == None): 
-            print("List is empty") 
-            return
-        temp = self.tail.next
-        while temp: 
-            print(temp.process, end=" ") 
-            temp = temp.next
-            if temp == self.tail.next: 
-                break
+      def print(self): 
+            # print the list
+            if (self.tail == None): 
+                  print("List is empty") 
+                  return
+            temp = self.tail.next
+            while temp: 
+                  print(temp.process, end=" ") 
+                  temp = temp.next
+                  if temp == self.tail.next: 
+                        break
+      
+      def getCount(self):
+            # get the length of the list
+            temp = self.tail.next
+            count = 0     
+
+            while(temp):
+                count += 1
+                temp = temp.next
+            return count 
+      
+
+      def track(self):
+            # set hands to starting positions
+            bighand = self.tail.next
+            length = self.getCount()
+            temp = self.tail.next
+            i = 0
+            for i in range(1, length/2):
+                temp = temp.next
+            smallhand = temp
+
+            # Check nodes and move hands
+            while i < self.getCount()*2:
+                  #split address and get the first two bits for smallhand
+                  addr = id(smallhand)
+                  binAddr = bin(addr)
+                  bits = binAddr[2:4]
+                  # check accessed and modified and advance hand
+                  if bits != "11":
+                        self.resetlist.append(smallhand)
+                  smallhand = smallhand.next
+                  # big hand checking if the oldest point of the reset 
+                  # list is equal to the node its checking.
+                  if bighand == self.resetlist[0]:
+                        # swap with new process
+                        return
+                  bighand = bighand.next
 
 
 class Node:
@@ -41,10 +84,3 @@ class Node:
       def __init__(self, process):
             self.process = process
             self.next = None
-
-
-if __name__ == '__main__': 
-    llist = CircularLinkedList() 
-    llist.traverse() 
-    last = llist.addNode((222, 8000))
-    llist.traverse() 
